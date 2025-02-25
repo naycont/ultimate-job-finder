9c868c81d8e65f6daa5d9f8bcf17887cd557bc1a
Nayeli Contreras <naycont@gmail.com>
2025-02-25 15:44:01 -0600

    fixes on vite-config to publish page

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
+  base: '/ultimate-job-finder/', // Base URL for the app
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'dist', // Output directory
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html') // Entry point for the build
     }
   }
 })
